2093faad47313f14c49cce6fddbaaf0aa369b8ff
Rommel Haynes <rhaynes3k@gmail.com>
2019-09-23 04:51:11 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
-def create_an_empty_array
-  
+create_an_empty_array
+  []
 end
 
-def create_an_array
-  
+create_an_array
+  [apple, orange, pear, plum]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array.push(create_an_array, peach)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array.unshift(create_an_array, grapes)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_element_from_end_of_array.pop(create_an_array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array.shift(create_an_array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_element_from_index(create_an_array, [1])
 end
 
 def retrieve_first_element_from_array(array)
-  
+  retrieve_first_element_from_array(create_an_array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array(create_an_array[-1])
 end
 
 def update_element_from_index(array, index_number, element)
-
+  update_element_from_index(create_an_array, [2], tomato)
 end
